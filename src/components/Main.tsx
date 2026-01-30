fefd65555d3a0e8c099d1b38da7952a3f4bc7ae5
Jelidah <153504634+Jelidah-Nayingwa@users.noreply.github.com>
2026-01-30 14:29:35 -0500

    updated github link

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -18,14 +18,14 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/JelidahJay" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/Jelidah" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/jelidah-nayingwa-9422241a6/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Jelidah Nayingwa</h1>
           <p>Full Stack Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/JelidahJay" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/Jelidah" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/jelidah-nayingwa-9422241a6/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
